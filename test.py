23da81181123429f81e67916aa411f034df8bd3d
ginger <19902512605@163.com>
2024-08-03 09:49:46 +0800

    8/3

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 from ultralytics import YOLO
 import cv2
-<<<<<<< Updated upstream
-model = YOLO("yolov10n-pghost-pose.yaml",task="pose").load("yolov10n-ghost-200epochs.pt")
-model.train(data="../datasets/rune/rune.yaml", epochs=100, imgsz=640)
-=======
+
 model = YOLO("yolov10n-doubleconv-pose.yaml",task="pose").load("best.pt")
 model.train(data="../datasets/rune/rune.yaml", epochs=300, imgsz=416)
 model.export(format="onnx")
->>>>>>> Stashed changes
